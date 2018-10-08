2d233b239205818976d68f7b7e6cbfdaaaa93383
Zhang Yunfeng <578210452@qq.com>
2018-10-08 11:57:54 +0800

    change

--- bin/oeepush_smtout.bat
+++ bin/oeepush_smtout.bat
@@ -2,6 +2,6 @@
 %~d0
 cd %~dp0 
 cd .. 
-phantomjs.exe %cd%\html2base64\rasterize_out.js %1 %2 5000 #containeroutput
+phantomjs.exe %cd%\html2base64\rasterize_out.js %1 %2 30000 #containeroutput
 pngquant --force --verbose --quality=45-85 %2
 exit /b 1
