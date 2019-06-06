03dc9ad138a39e5c7f43e621775bb48d0897a8ab
Zhang Yunfeng <578210452@qq.com>
2019-06-06 11:54:59 +0800

    change

--- bin/oeepush.bat
+++ bin/oeepush.bat
@@ -2,4 +2,4 @@
 %~d0
 cd %~dp0 
 cd ..
-phantomjs.exe %cd%\html2base64\rasterize.js http://w3uy006a:8089/report/one_page_report_line?lineno=%1 6000 #report_table
+phantomjs.exe %cd%\html2base64\rasterize.js http://w3uy006a:8089/report/one_page_report_line?lineno=%1 7000 #report_table
--- bin/oeepush_out.bat
+++ bin/oeepush_out.bat
@@ -2,6 +2,6 @@
 %~d0
 cd %~dp0 
 cd .. 
-phantomjs.exe %cd%\html2base64\rasterize_out.js %1 %2 6000 #report_table 
+phantomjs.exe %cd%\html2base64\rasterize_out.js %1 %2 7000 #report_table 
 pngquant --force --verbose --quality=45-85 %2
 exit /b 1
